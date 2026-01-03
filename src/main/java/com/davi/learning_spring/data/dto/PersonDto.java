30babf39173f88a56800a6cf08fee2bf9026560c
DaviAlencar2 <davilt.alencar@gmail.com>
2026-01-02 21:12:22 -0300

    1202 Custom JSON Serialization

--- src/main/java/com/davi/learning_spring/data/dto/PersonDto.java
+++ src/main/java/com/davi/learning_spring/data/dto/PersonDto.java
@@ -1,15 +1,26 @@
 package com.davi.learning_spring.data.dto;
 
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import lombok.Data;
 
+
+// A maneira como o proprio spring e a jvm define a ordem de exibicao dos campos do json é um pouco confusa e dependendo do contexto dificil de prever para isso usar essa funcionalidade do jackson garante a ordem desejada de exibicao dos campos
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"}) //Jackson 2.12+ (Spring Boot 2.5+, incluindo Spring Boot 3.x)Você pode usar AMBOS - tanto o nome da variável Java quanto o nome JSON customizado:
 @Data
 public class PersonDto implements Serializable{
     private static final long serialVersionUID = 1L;
     
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+    @JsonIgnore
     private String address;
     private String gender;
 
